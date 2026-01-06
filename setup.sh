e0e7a7177ba5a6eaafbc43fe19dbad38414c2a1a
AnonymousUserPublish <anonymous.user.publish@gmail.com>
2026-01-06 20:30:19 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ pip install wandb==0.18.3
 pip install tensorboardx
 pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
 pip install qwen_vl_utils 
-pip install flash-attn --no-build-isolation
+pip install --extra-index-url https://miropsota.github.io/torch_packages_builder flash_attn==2.8.3+pt2.6.0cu124
 pip install transformers==4.51.3
