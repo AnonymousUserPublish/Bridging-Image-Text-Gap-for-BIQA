b4ffbf75e611779bba7cf8dffef212ec2e01e31d
AnonymousUserPublish <anonymous.user.publish@gmail.com>
2026-01-06 18:51:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ pip install -e ".[dev]"
 # Addtional modules
 pip install wandb==0.18.3
 pip install tensorboardx
-pip install qwen_vl_utils torchvision
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu124
+pip install qwen_vl_utils 
 pip install flash-attn --no-build-isolation
 pip install transformers==4.51.3
